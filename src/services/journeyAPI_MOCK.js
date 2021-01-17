8c02852d17eeeb2c53d8e671f722d97c093909bd
Samuel Caetité <samuel.caetite@vorttex.com.br>
2021-01-16 21:55:22 -0300

    feat: criados os arquivos iniciais da jornada

---
+++ src/services/journeyAPI_MOCK.js
@@ -0,0 +1,50 @@
+const journey = [
+	{
+		id: 0,
+		name: "Registro",
+		lessons:  [
+			{
+				id: 0,
+				isDone: false,
+				name: "Opções",
+			}
+		]
+	},
+	{
+		id: 1,
+		name: "Primeiros passos",
+		lessons: [
+
+		]
+	},
+	{
+		id: 2,
+		name: "Fazendo contas",
+		lessons: [
+
+		]
+	},
+	{
+		id: 3,
+		name: "Entendendo o público",
+		lessons: [
+
+		]
+	},
+	{
+		id: 4,
+		name: "Botando a mão na massa",
+		lessons: [
+
+		]
+	},
+	{
+		id: 5,
+		name: "Vendendo e consultando",
+		lessons: [
+
+		]
+	},
+]
+
+export default journey;
